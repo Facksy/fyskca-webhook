5495ac7665f5a25514a05e3b8ab5da5efc255925
lucas.thieux <lucas.thieux@gmail.com>
2021-06-05 16:37:35 +0200

    2nd

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-let x = 0
+const http = require('http')
+const port = 8000;
+
+const server = http.createServer((req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    req.setEncoding('utf8');
+    req.on('data', chunk => {
+        console.log('A chunk of data has arrived: ', chunk);
+      });
+}).listen(8000);
+
+
+/*
+
+const data = JSON.stringify({
+  todo: 'Buy the milk'
+})
+
+const options = {
+  hostname: 'localhost',
+  port: 8000,
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': data.length
+  }
+}
+
+const req = http.request(options)
+req.write(data)
+req.end()
+*/
