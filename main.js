2101789f25c2716f4064da944b01677c02012734
lucas.thieux <lucas.thieux@gmail.com>
2021-06-05 17:10:01 +0200

    okk

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const http = require('http')
+/*const http = require('http')
 const port = 8000;
 
 const server = http.createServer((req, res) => {
@@ -6,21 +6,20 @@ const server = http.createServer((req, res) => {
     req.setEncoding('utf8');
     req.on('data', chunk => {
         console.log('A chunk of data has arrived: ', chunk);
-      });
-}).listen(8000);
+    });
+    console.log('received a request');
+}).listen(5545, '192.168.0.14');
 
 console.log('ready!');
 
-console.log(server.address());
-/*
 
 const data = JSON.stringify({
   todo: 'Buy the milk'
 })
 
 const options = {
-  hostname: 'localhost',
-  port: 8000,
+  hostname: '93.5.162.60',
+  port: 5545,
   method: 'POST',
   headers: {
     'Content-Type': 'application/json',
@@ -30,5 +29,22 @@ const options = {
 
 const req = http.request(options)
 req.write(data)
-req.end()
-*/
+req.end()*/
+
+const { networkInterfaces } = require('os');
+
+const nets = networkInterfaces();
+const results = Object.create(null); // Or just '{}', an empty object
+
+for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+        // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+        if (net.family === 'IPv4' && !net.internal) {
+            if (!results[name]) {
+                results[name] = [];
+            }
+            results[name].push(net.address);
+        }
+    }
+}
+console.log(results);
