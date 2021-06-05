15cd873193693f64b4a5115c8ea01a54b3d67322
lucas.thieux <lucas.thieux@gmail.com>
2021-06-05 22:17:16 +0200

    cca

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const http = require('https')
+const http = require('http')
 
 const server = http.createServer((req, res) => {
     res.setHeader("Content-Type", "application/json");
@@ -7,27 +7,27 @@ const server = http.createServer((req, res) => {
         console.log('A chunk of data has arrived: ', chunk);
     });
     console.log('received a request');
-}).listen(() => {  // 172.18.192.138 192.168.0.14
-    console.log('Listening on port ', server.address());
+}).listen(5545, '192.168.0.14', () => {
+    console.log('Listening on ', server.address());
 });
 
 console.log('ready!');
 
-/*
+
 const data = JSON.stringify({
   todo: 'Buy the milk'
 })
 
 const options = {
-  hostname: 'https://webhook.site/6637fadd-ae2b-4eb9-aaa6-bbfd769aed2f',
+  hostname: '93.5.162.60',
   method: 'POST',
+  port: '5545',
   headers: {
     'Content-Type': 'application/json',
     'Content-Length': data.length
   }
 }
-
+/*
 const req = http.request(options)
 req.write(data)
-req.end()
-*/
+req.end()*/
