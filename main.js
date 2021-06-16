0be50d07c0e6f6f082eae01b080609c718264948
Facksy <45769049+Facksy@users.noreply.github.com>
2021-06-16 15:38:29 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,9 @@
-const http = require('http')
+const http = require('http');
 
-const server = http.createServer((req, res) => {
-    res.setHeader("Content-Type", "application/json");
-    req.setEncoding('utf8');
-    req.on('data', chunk => {
-        console.log('A chunk of data has arrived: ', chunk);
-    });
-    console.log('received a request');
-}).listen(5545, '192.168.0.14', () => {
-    console.log('Listening on ', server.address());
-});
-
-console.log('ready!');
-
-
-const data = JSON.stringify({
-  todo: 'Buy the milk'
-})
-
-const options = {
-  hostname: '93.5.162.60',
-  method: 'POST',
-  port: '5545',
-  headers: {
-    'Content-Type': 'application/json',
-    'Content-Length': data.length
-  }
+const requestListener = (req, res) => {
+    res.writeHead(200);
+    res.end('Hello, World!');
 }
-/*
-const req = http.request(options)
-req.write(data)
-req.end()*/
+
+const server = http.createServer(requestListener);
+server.listen(80);
