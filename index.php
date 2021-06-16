b89c45d2b37c391a2d41ee5f2e575091efa0d2d7
lucas.thieux <lucas.thieux@gmail.com>
2021-06-16 16:15:15 +0200

    eacea

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<script>
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", 'http://localhost:8000', false ); // false for synchronous request
+    xmlHttp.send( null );
+    alert(xmlHttp.responseText);
+    alert('done')
+}
+</script>
+
 <?php
-    echo "Hellow";
+    echo "Hellow <button onclick='httpGet()'>Test</button>";
 ?>
