aefeccad9dd64c66d2581d64e3b6ff655c1c3641
lucas.thieux <lucas.thieux@gmail.com>
2021-06-26 13:43:26 +0200

    dea

--- main.js
+++ main.js
@@ -2,7 +2,4 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
     res.end('dadata')
-});
-server.listen(process.env.PORT, () => {
-    console.log('Server is running on ', server.address());
-});
+}).listen(process.env.PORT);
